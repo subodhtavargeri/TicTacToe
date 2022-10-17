74ce7a24d4442f324e4a86f128d44f568a97f8e5
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-10-17 12:03:14 +0530

    refactor code

--- TicTacToe/Game/Game.swift
+++ TicTacToe/Game/Game.swift
@@ -10,7 +10,7 @@ class Game {
     private (set) var boardArray = [String]()
     private (set) var gameFinished = false
     
-    let winningRules = [[0,1,2],[3,4,5],
+    private let winningRules = [[0,1,2],[3,4,5],
                         [6,7,8],[0,3,6],
                         [1,4,7],[2,5,8],
                         [0,4,8],[2,4,6]]
